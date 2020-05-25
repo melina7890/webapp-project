a3dfc9655ab098ebbc55c8c4a03c4dad8e2114ae
melina7890 <melina7890@gmail.com>
2020-05-25 15:21:25 -0400

    init commit

---
+++ webapp/models.py
@@ -0,0 +1,124 @@
+from webapp import db
+import pandas as pd
+from sqlalchemy.sql import func
+
+class Cancer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cancer_type = db.Column(db.String(100), unique=True, nullable=False)
+
+    #cancer_id =
+    def __repr__(self):
+        return f"Cancer Type('{self.cancer_type}')"
+
+class Intervention(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    intervention_type = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Drug Type('{self.intervention_type}')"
+#
+# class Outcome(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     outcome_name = db.Column(db.String(100), unique=True, nullable=False)
+#
+class Association(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    association = db.Column(db.String(40), unique=True, nullable=False)
+    def __repr__(self):
+        return f"Association('{self.association}')"
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pmid = db.Column(db.Integer, nullable=False, unique=True)
+    title = db.Column(db.String(500))
+    abstract = db.Column(db.String(8,000))
+    study_type = db.Column(db.String(100))
+    cancer = db.Column(db.String(100)) # db.ForeignKey('cancer.id')
+    intervention = db.Column(db.String(100)) #db.ForeignKey('intervention.id')
+    association = db.Column(db.String(40)) # association = db.Column(db.Integer db.ForeignKey('association.id'))\
+    def __repr__(self):
+        return f"Article('{self.pmid}', '{self.title}',  '{self.cancer}', '{self.intervention}')"
+
+class Rank(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cancer = db.Column(db.String(100))
+    intervention = db.Column(db.String(100))
+    effective = db.Column(db.Integer, nullable=False, default=0)
+    inconclusive = db.Column(db.Integer, nullable=False, default=0)
+    detrimental = db.Column(db.Integer, nullable=False, default=0)
+    all = db.Column(db.Integer, nullable=False, default=0)
+    rank = db.Column(db.Float, nullable=False, default=0)
+
+    def __init__(self, **kwargs):
+        super(Rank, self).__init__(**kwargs)
+        self.rank = self.effective - self.detrimental;
+
+    def __repr__(self):
+        return f"Rank('{self.intervention}', '{self.cancer}')"
+
+#  Scoring table separate table for cancer,drug, and counts
+
+db.create_all()
+
+c_df = pd.read_csv("webapp/cancers.csv", header=None)
+#import pdb; pdb.set_trace()
+cancers = c_df[0].values
+for c in cancers:
+    not_exists = db.session.query(
+    Cancer.cancer_type
+    ).filter(func.lower(Cancer.cancer_type)==func.lower(c)).scalar() is None
+    if not_exists:
+        new_c = Cancer(cancer_type=c)
+        db.session.add(new_c)
+
+d_df = pd.read_csv("webapp/drugs.csv", header=None)
+#import pdb; pdb.set_trace()
+drugs = d_df[0].values
+for d in drugs:
+    not_exists = db.session.query(
+    Intervention.intervention_type
+    ).filter(func.lower(Intervention.intervention_type)==func.lower(d)).scalar() is None
+    if not_exists:
+        new_d = Intervention(intervention_type=d)
+        db.session.add(new_d)
+
+article_df = pd.read_csv("webapp/articles.csv", header=0)
+for index, row in article_df.iterrows():
+    #if (c_id is not None and d_id is not None):
+    not_exists = db.session.query(Article).filter_by(pmid=row.pmid,
+    cancer=row.disease, intervention=row.drugs).scalar() is None
+
+    if not_exists:
+        #import pdb; pdb.set_trace()
+        new_a = Article(pmid=row.pmid, title=row.title, abstract=row.abstract,
+        study_type=row['study type'], cancer=row.disease, intervention=row.drugs,
+        association=row.association)
+
+
+        db.session.add(new_a)
+
+# populate the rank table
+# currently we're dealing with dummy data so no article matches
+for c in cancers:
+    for d in drugs:
+        #import pdb; pdb.set_trace()
+        all = Article.query.filter(Article.cancer.ilike(c)).filter(Article.intervention.ilike(d))
+        effective = all.filter(Article.association.ilike("Effective")).count();
+        detrimental = all.filter(Article.association.ilike("Detrimental")).count();
+        inconclusive = all.filter(Article.association.ilike("No effect")).count();
+
+        not_exists = db.session.query(Rank).filter(
+                func.lower(Rank.cancer)==func.lower(c), func.lower(Rank.intervention)==func.lower(d)
+            ).scalar() is None
+        if not_exists:
+            new_rank = Rank(
+            all=all.count(),
+            cancer=c,
+            intervention=d,
+            effective=effective,
+            inconclusive=inconclusive,
+            detrimental=detrimental
+            )
+            db.session.add(new_rank)
+
+db.session.commit();
---
+++ webapp/static/sidePanel.js
@@ -0,0 +1,128 @@
+var drugs = [];
+var cancers = [];
+
+function filterResults() {
+ $.ajax({
+      url : "/home",
+      type : "POST",
+      data : { drugs : JSON.stringify(drugs), cancers: JSON.stringify(cancers) }
+
+  }).done(function(data) {
+      window.location.reload();
+      console.log("done");
+  })
+  .fail(function(err) {
+      console.log("fail");
+  });
+}
+
+function addTags(type, tag) {
+  tag = tag.toLowerCase();
+  if (type == 'cancer') {
+    cancers.push(tag);
+  } else if (type == 'drug') {
+    drugs.push(tag);
+  }
+}
+
+function removeTags(type, tag) {
+  tag = tag.toLowerCase();
+  if (type == 'cancer') {
+    removeFromArr(cancers, tag);
+  } else if (type == 'drug') {
+    removeFromArr(drugs, tag);
+  }
+}
+
+function removeFromArr(arr, val) {
+    let index = arr.indexOf(val);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+}
+
+// Update the tags that exist when a user interacts with the checkboxes
+function update_tags(id, type) {
+  // Get the checkbox
+  const checkBox = document.getElementById(id);
+  const tag = checkBox.getAttribute('data');
+
+  let btnId = "btn" + id;
+  let btnElement = document.getElementById(btnId);
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true && (typeof(btnElement) == 'undefined' || btnElement == null)){
+    let element = document.createElement("button");
+    element.setAttribute("id", btnId);
+    element.setAttribute("class", "filter-button");
+    element.setAttribute("data-type", type);
+    element.setAttribute("onclick", "removeButton(id)");
+    element.innerHTML = tag;
+
+    // the close icon
+    let icon = document.createElement("i");
+    icon.setAttribute("class", "fa fa-close x-icon");
+    element.appendChild(icon);
+
+    document.getElementById("tags").appendChild(element);
+
+    addTags(type, tag);
+
+  } else if (checkBox.checked == false && btnElement != null) {
+    btnElement.remove();
+    removeTags(type, tag);
+  }
+  filterResults();
+}
+
+// remove the tag button- cancer and drug
+function removeButton(id) {
+  const typeId = id.replace('btn','');
+  const checkBox = document.getElementById(typeId);
+  const tag = checkBox.getAttribute('data');
+  const btnElement = document.getElementById(id);
+  type = btnElement.getAttribute("data-type");
+  // remove button and uncheck if box is checked
+  checkBox.checked = false;
+  btnElement.remove();
+
+  removeTags(type, tag);
+  filterResults();
+}
+
+function searchCancer() {
+  let input, filter, cancerList, list, label, txtValue;
+  input = document.getElementById("search-cancer");
+  filter = input.value.toLowerCase();
+  cancerList = document.getElementById("cancer-list");
+  list = cancerList.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < list.length; i++) {
+    label = list[i].getElementsByTagName("label")[0];
+    txtValue = label.textContent || label.innerText;
+    if (txtValue.toLowerCase().indexOf(filter) > -1) {
+      list[i].style.display = "";
+    } else {
+      list[i].style.display = "none";
+    }
+  }
+}
+
+function searchDrug() {
+  let input, filter, drugList, list, label, txtValue;
+  input = document.getElementById("search-drug");
+  filter = input.value.toLowerCase();
+  drugList = document.getElementById("drug-list");
+  list = drugList.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < list.length; i++) {
+    label = list[i].getElementsByTagName("label")[0];
+    txtValue = label.textContent || label.innerText;
+    if (txtValue.toLowerCase().indexOf(filter) > -1) {
+      list[i].style.display = "";
+    } else {
+      list[i].style.display = "none";
+    }
+  }
+}
